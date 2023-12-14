e3a13347acbab5488d3f5375f43533da88be62fc
Ramyalakshmi <ramy12797@gmail.com>
2023-12-14 11:36:08 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ const About = () => {
           <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
           <p>
             Hello, my name is <b>Ramya</b> and I am from Chennai, India.
-            I'm a <b>Java backend developer</b> and pursuing <b>MS in CSE</b>. <br/><br/>
+            I'm a <b>Java backend developer</b> and pursuing <b>MS in Computer Science</b>. <br/><br/>
             In my past role as a <b> software engineer</b>, I led the backend team, improved system efficiency, managed the database lifecycle, and received recognition for cost-saving implementations.
             <br></br><br></br>I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
             Besides coding, I love to sing.<br/>
